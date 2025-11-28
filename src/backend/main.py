0e7d2dc65a9b952e5805725740828ff3900ff1dc
Quân <minhquan210205@gmail.com>
2025-11-29 04:34:22 +0700

    chinh code backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -9,10 +9,10 @@ app = FastAPI()
 filtered_data = []
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Cho phép TẤT CẢ các trang web truy cập (bao gồm Vercel)
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Cho phép tất cả các method (GET, POST...)
+    allow_headers=["*"],  # Cho phép tất cả các header
 )
 model_path = r"D:\nam4\AI_rubbish\runs\detect\multitrash_28_classes7\weights\best.pt"
 try:
